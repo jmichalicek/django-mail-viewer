850b2b543b6529321036e8a48c8c34461cd52acd
Justin Michalicek <jmichalicek@gmail.com>
2021-02-15 01:52:16 +0000

    Stop testing on unsupported Django and Pythons
    * Stopped testing on Python 3.5 even for Django versions which
    technically support it. Python 3.5 itself is no longer supported.
    * Stopped testing on Django versions less than 2.2 which are all no
    longer supported.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import os
 import sys
 
@@ -44,17 +43,13 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
