23023654c7a410b351d47e55a195b233c1c9c6a8
Justin Michalicek <jmichalicek@gmail.com>
2021-02-21 17:59:09 +0000

    Swappable model for database backend
    * Made the model used for database backend email storage a setting and
    loaded at runtime. This allows users to use their own model, as the docs
    suggest for certain use cases, and allows the backend to be referenced
    in the same way the others are without the extra package
    name/namespacing.
    * Normalized usage of mailviewer vs mail_viewer in some places
    * Moved a bunch of methods from EmailMessage to AbstractBaseEmailMessage
    * Minor type hinting improvements

--- django_mail_viewer/backends/database/apps.py
+++ django_mail_viewer/backends/database/apps.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class DatabaseBackendConfig(AppConfig):
-    label = 'mailviewer_database_backend'
+    label = 'mail_viewer_database_backend'
     name = 'django_mail_viewer.backends.database'
     # May want to change this verbose_name
     verbose_name = _("Database Backend")
--- django_mail_viewer/settings.py
+++ django_mail_viewer/settings.py
@@ -5,3 +5,4 @@ from django.conf import settings
 # The cache config from django.core.cache.caches to use for backends.cache.CacheBackend
 # default to django.core.cache.caches['default']
 MAILVIEWER_CACHE = getattr(settings, 'MAILVIEWER_CACHE', 'default')
+MAILVIEWER_DATABASE_BACKEND_MODEL = getattr(settings, 'MAILVIEWER_DATABASE_BACKEND_MODEL', 'mail_viewer_database_backend.EmailMessage')
